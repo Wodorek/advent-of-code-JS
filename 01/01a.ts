67e54e95b6809887c80104ae755659df659c4d44
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-12-03 02:15:32 +0100

    Update 01a.ts
    Removed redundant variables

--- 01/01a.ts
+++ 01/01a.ts
@@ -2,14 +2,12 @@ import input from './inputFile';
 
 const dataArr = input.split('\n');
 
-let idx = 0;
 let times = 0;
 
 for (let i = 0; i < dataArr.length; i++) {
-  if (+dataArr[idx + 1] > +dataArr[idx]) {
+  if (+dataArr[i + 1] > +dataArr[i]) {
     times++;
   }
-  idx++;
 }
 
 console.log(times);
