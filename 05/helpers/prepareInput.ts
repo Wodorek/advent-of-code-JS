7b6e5f453f199fe640536539ee1edb01f66180b4
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-12-05 18:46:37 +0100

    JSDoc

--- 05/helpers/prepareInput.ts
+++ 05/helpers/prepareInput.ts
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param str an input string in a format of "x1,y1 -> x2,y2", each value separated by newline
+ * @returns an array of arrays containing values: [[x1, y1],[x2,y2]]
+ */
 const prepareInput = (str: string) => {
   return str.split('\n').map((el) => {
     const split = el.split(' -> ');
