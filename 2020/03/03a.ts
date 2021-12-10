b1df7c0ec18337f5973e061cf5c5cf68748e1516
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-12-10 16:28:45 +0100

    2020 day 3 part 1

---
+++ 2020/03/03a.ts
@@ -0,0 +1,30 @@
+import prepareInput from './helpers/prepareInput';
+import input from './input';
+
+const inputArr = prepareInput(input);
+
+console.log(inputArr);
+
+const lineLength = inputArr[0].length;
+console.log(lineLength);
+
+const slopeHeight = inputArr.length - 1;
+let posX = 0;
+let posY = 0;
+
+let trees = 0;
+
+while (posY < slopeHeight) {
+  posY++;
+  posX += 3;
+
+  if (posX > lineLength - 1) {
+    posX = posX - lineLength;
+  }
+
+  if (inputArr[posY][posX] === '#') {
+    trees++;
+  }
+}
+
+console.log(trees);
