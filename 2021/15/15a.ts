c88004f3fe5f938652559b26eae65823a5bf104d
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-12-15 15:52:30 +0100

    Update 15a.ts

--- 2021/15/15a.ts
+++ 2021/15/15a.ts
@@ -4,49 +4,55 @@ import neighbors from './helpers/neighbors';
 
 const inputArr = prepareInput(input);
 
-const shortestPath = (
-  length: number,
-  map: (y: number, x: number) => number
-): number => {
-  const dist = [];
-  const done = [];
-  for (let y = 0; y < length; ++y) {
-    const done_row = [];
-    const dist_row = [];
-    for (let x = 0; x < length; ++x) {
-      done_row[x] = 1;
-      dist_row[x] = Infinity;
-    }
-    done.push(done_row);
-    dist.push(dist_row);
-  }
-  dist[0][0] = 0;
-  done[0][0] = 2;
-  const queue = [[0, 0]];
-  while (queue.length > 0) {
-    let min_dist = Infinity;
-    let min_dist_idx = 0;
-    for (let i = 0; i < queue.length; ++i) {
-      const [yy, xx] = queue[i];
-      if (dist[yy][xx] < min_dist) {
-        min_dist = dist[yy][xx];
-        min_dist_idx = i;
-      }
-    }
-    const [y, x] = queue[min_dist_idx];
-    queue[min_dist_idx] = queue[queue.length - 1];
-    queue.length--;
-    done[y][x] = 3;
-    for (const [yy, xx] of neighbors(inputArr, y, x)) {
-      if (done[yy][xx] == 3) continue;
-      dist[yy][xx] = Math.min(dist[yy][xx], dist[y][x] + map(yy, xx));
-      if (done[yy][xx] == 1) {
-        done[yy][xx] = 2;
-        queue.push([yy, xx]);
-      }
-    }
-  }
-  return dist[length - 1][length - 1];
-};
+const distances = [];
+const visited = [];
 
-console.log(shortestPath(inputArr.length, (x, y) => inputArr[x][y]));
+for (let i = 0; i < inputArr.length; i++) {
+  const distancesRow = [];
+  const visitedRow = [];
+
+  for (let j = 0; j < inputArr[0].length; j++) {
+    distancesRow.push(Infinity);
+    visitedRow.push(1);
+  }
+
+  distances.push(distancesRow);
+  visited.push(visitedRow);
+}
+
+const queue = [[0, 0]];
+//do not factor in value of start, since you never enter it
+distances[0][0] = 0;
+visited[0][0] = 0;
+
+while (queue.length > 0) {
+  let minDistance = Infinity;
+  let minDistanceIdx = 0;
+  for (let i = 0; i < queue.length; ++i) {
+    const [row, column] = queue[i];
+    if (distances[row][column] < minDistance) {
+      minDistance = distances[row][column];
+      minDistanceIdx = i;
+    }
+  }
+
+  const [y, x] = queue[minDistanceIdx];
+  queue[minDistanceIdx] = queue[queue.length - 1];
+  queue.length--;
+
+  visited[y][x] = 3;
+
+  for (const [yy, xx] of neighbors(inputArr, y, x)) {
+    if (visited[yy][xx] === 3) continue;
+    distances[yy][xx] = Math.min(
+      distances[yy][xx],
+      distances[y][x] + inputArr[yy][xx]
+    );
+    if (visited[yy][xx] === 1) {
+      visited[yy][xx] = 2;
+      queue.push([yy, xx]);
+    }
+  }
+}
+
+console.log(distances[inputArr.length - 1][inputArr.length - 1]);
