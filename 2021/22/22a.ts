12d793d322ea41edaeab097e80563fd04f8e268f
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-12-22 13:24:52 +0100

    Update 22a.ts

--- 2021/22/22a.ts
+++ 2021/22/22a.ts
@@ -3,6 +3,8 @@ import prepareInput from './helpers/prepareInput';
 
 const inputArr = prepareInput(input);
 
+console.log(inputArr);
+
 const cubes: { [key: string]: number } = {};
 
 const bounds = [-50, 50];
@@ -11,22 +13,20 @@ for (let i = 0; i < inputArr.length; i++) {
   const coords = inputArr[i][1];
   const directive = inputArr[i][0];
 
-  console.log(i);
-
   for (let x = coords[0][0]; x < coords[0][1] + 1; x++) {
-    // if (coords[0][0] < bounds[0] || coords[0][1] > bounds[1]) {
-    //   continue;
-    // }
+    if (coords[0][0] < bounds[0] || coords[0][1] > bounds[1]) {
+      continue;
+    }
 
     for (let y = coords[1][0]; y < coords[1][1] + 1; y++) {
-      // if (coords[1][0] < bounds[0] || coords[1][1] > bounds[1]) {
-      //   continue;
-      // }
+      if (coords[1][0] < bounds[0] || coords[1][1] > bounds[1]) {
+        continue;
+      }
 
       for (let z = coords[2][0]; z < coords[2][1] + 1; z++) {
-        // if (coords[2][0] < bounds[0] || coords[2][1] > bounds[1]) {
-        //   continue;
-        // }
+        if (coords[2][0] < bounds[0] || coords[2][1] > bounds[1]) {
+          continue;
+        }
 
         const key = `${x},${y},${z}`;
 
