9953b2656d44eb9c0b29f3538dc9dd2823e5f52c
Wodorek <Krol.w94@gmail.com>
2021-12-23 16:10:27 +0100

    2020 day 10 part 1

---
+++ 2020/10/10a.ts
@@ -0,0 +1,31 @@
+import input from './input';
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+let sorted = inputArr.sort((a, b) => {
+  return a > b ? 1 : -1;
+});
+
+sorted = [0, ...sorted];
+
+let left = 0;
+let right = 1;
+
+const joltDiffs: { [key: number]: number } = {
+  3: 1,
+};
+
+while (right < sorted.length) {
+  const difference = sorted[right] - sorted[left];
+
+  if (joltDiffs[difference]) {
+    joltDiffs[difference]++;
+  } else {
+    joltDiffs[difference] = 1;
+  }
+  left++;
+  right++;
+}
+
+console.log(joltDiffs[1] * joltDiffs[3]);
