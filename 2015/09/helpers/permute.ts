168aab4b7e98a6e310c4438ac4a303b34389daca
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-04 18:58:21 +0100

    2015 day 9 part 1

---
+++ 2015/09/helpers/permute.ts
@@ -0,0 +1,17 @@
+const permute = (xs: string[]) => {
+  let ret = [];
+
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest: string[][] = permute(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if (!rest.length) {
+      ret.push([xs[i]]);
+    } else {
+      for (let j = 0; j < rest.length; j = j + 1) {
+        ret.push([xs[i]].concat(rest[j]));
+      }
+    }
+  }
+  return ret;
+};
+export default permute;
