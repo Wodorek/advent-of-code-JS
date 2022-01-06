c110a1e91800509af58b323ef0209091d1944b37
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-06 17:07:36 +0100

    2015 day 16 part 2

---
+++ 2015/16/16b.ts
@@ -0,0 +1,46 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+const presentAunt = {
+  children: 3,
+  cats: 7,
+  samoyeds: 2,
+  pomeranians: 3,
+  akitas: 0,
+  vizslas: 0,
+  goldfish: 5,
+  trees: 3,
+  cars: 2,
+  perfumes: 1,
+};
+
+let matchingAunts = inputArr;
+
+Object.keys(presentAunt).forEach((key) => {
+  matchingAunts = matchingAunts.filter((aunt) => {
+    const auntKeys = Object.keys(aunt);
+    const idx = key as keyof typeof presentAunt;
+
+    if (!auntKeys.includes(key)) {
+      return true;
+    }
+
+    if (key === 'cats' || key === 'trees') {
+      return aunt[key] > presentAunt[idx] ? true : false;
+    }
+
+    if (key === 'pomeranians' || key === 'goldfish') {
+      return aunt[key] < presentAunt[idx] ? true : false;
+    }
+
+    if (aunt[key] === presentAunt[idx]) {
+      return true;
+    }
+    return false;
+  });
+});
+
+console.log(matchingAunts[0].id);
