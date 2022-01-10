d95e2b5322fecb4162f5f5300badcb2a650fba74
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-10 19:54:36 +0100

    2016 day 7 part 1

---
+++ 2016/07/07a.ts
@@ -0,0 +1,55 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+const checkTSLsupport = (ip: string) => {
+  let left = 0;
+  let leftEnd = 1;
+  let right = 2;
+  let rightEnd = 3;
+
+  let canSupport = false;
+
+  while (left < ip.length) {
+    if (ip[left] !== ip[leftEnd]) {
+      const leftPart = `${ip[left]}${ip[leftEnd]}`;
+      const rightPart = `${ip[rightEnd]}${ip[right]}`;
+
+      if (leftPart === rightPart) {
+        let checker = left - 1;
+
+        while (checker >= 0) {
+          const item = ip[checker];
+
+          if (item === ']') {
+            break;
+          }
+
+          if (item === '[') {
+            return false;
+          }
+          checker--;
+        }
+        canSupport = true;
+      }
+    }
+    left++;
+    leftEnd++;
+    right++;
+    rightEnd++;
+  }
+
+  return canSupport;
+};
+
+let supportingCount = 0;
+
+inputArr.forEach((ip) => {
+  if (checkTSLsupport(ip)) {
+    supportingCount++;
+  }
+});
+
+console.log(supportingCount);
