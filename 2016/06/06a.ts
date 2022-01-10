857e908a7eb0768e10306d3f89e5edc7bad9fc04
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-10 19:24:59 +0100

    2016 day 06 part 1

---
+++ 2016/06/06a.ts
@@ -0,0 +1,36 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+console.log(inputArr);
+
+const counts: { [key: number]: { [key: string]: number } } = {};
+
+for (let i = 0; i < inputArr[0].length; i++) {
+  counts[i] = {};
+
+  inputArr.forEach((line) => {
+    const char = line[i];
+
+    if (counts[i][char]) {
+      counts[i][char]++;
+    } else {
+      counts[i][char] = 1;
+    }
+  });
+}
+
+let message = '';
+
+for (let i = 0; i < Object.keys(counts).length; i++) {
+  const values = Object.values(counts[i]);
+  const highestValue = Math.max(...values);
+
+  const valueIdx = values.indexOf(highestValue);
+
+  message = message.concat(Object.keys(counts[i])[valueIdx]);
+}
+
+console.log(message);
