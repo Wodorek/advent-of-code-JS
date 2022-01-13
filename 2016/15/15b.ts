2eb9d8415d30e1d6225cb058cdf2eb83764dcde4
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-13 16:29:02 +0100

    2016 day 15 part  2

---
+++ 2016/15/15b.ts
@@ -0,0 +1,71 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+const initialDiscs: { [key: string]: { positions: number; current: number } } =
+  {};
+
+inputArr.forEach((disc) => {
+  initialDiscs[disc[0]] = { positions: disc[1], current: disc[2] };
+});
+
+const advanceDiscs = (
+  discs: {
+    [key: string]: { positions: number; current: number };
+  },
+  by: number
+) => {
+  const discsKeys = Object.keys(discs);
+
+  //can you find a more dirty way to copy an object?
+  const newDiscs = JSON.parse(JSON.stringify(discs));
+
+  discsKeys.forEach((key) => {
+    let newIdx = (newDiscs[key].current + by) % newDiscs[key].positions;
+
+    if (newIdx < 0) {
+      newIdx = newDiscs[key].positions + newIdx;
+    }
+
+    newDiscs[key].current = newIdx;
+  });
+
+  return newDiscs;
+};
+
+const canWin = (
+  discs: {
+    [key: string]: { positions: number; current: number };
+  },
+  idx: number
+) => {
+  discs = advanceDiscs(discs, idx + 1);
+
+  for (let j = 1; j < Object.keys(discs).length + 1; j++) {
+    if (discs[j].current !== 0) {
+      return false;
+    }
+    discs = advanceDiscs(discs, 1);
+  }
+
+  return true;
+};
+
+let currTime = 0;
+
+while (!canWin(initialDiscs, currTime)) {
+  currTime++;
+}
+
+console.log(currTime);
+
+initialDiscs[7] = { positions: 11, current: 0 };
+
+currTime++;
+while (!canWin(initialDiscs, currTime)) {
+  currTime++;
+}
+
+console.log(currTime);
