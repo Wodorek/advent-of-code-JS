f44fb24446d0d31546347e903fa4da125867a5fe
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-18 16:54:40 +0100

    2016 day 23 part 1
    sneaky, sneaky jnz

---
+++ 2016/23/helpers/prepareInput.ts
@@ -0,0 +1,12 @@
+const prepareInput = (input: string) => {
+  return input.split('\n').map((el) => {
+    const split = el.split(' ');
+    if (split[0] === 'inc' || split[0] === 'dec' || split[0] === 'tgl') {
+      return [split[0], split[1]];
+    } else {
+      return [split[0], split[1], split[2]];
+    }
+  });
+};
+
+export default prepareInput;
