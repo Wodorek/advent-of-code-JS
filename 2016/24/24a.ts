789a7e25bf0a9a8275fe026d99c887deb3a3b4b5
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-19 03:04:09 +0100

    2016 day 24 part 1

--- 2016/24/24a.ts
+++ 2016/24/24a.ts
@@ -21,7 +21,7 @@ const findLocationCoordinates = (labirynth: string[]) => {
   }
 };
 
-let possibleRoutes = permute([0, 1, 2, 3, 4]);
+let possibleRoutes = permute([0, 1, 2, 3, 4, 5, 6, 7]);
 possibleRoutes = possibleRoutes.filter((el) => {
   return el[0] === 0;
 });
@@ -36,48 +36,41 @@ const bfs = (from: [number, number], to: [number, number]) => {
   const pathTaken: number[][][] = [];
 
   while (queue.length > 0) {
-    console.log(queue.length);
     const position = queue.pop()!;
 
-    const coordinates = `${position[0]},${position[1]}`;
-
-    visited.push(coordinates);
-
-    const possibleMoves = neighbors(inputArr, position);
+    const possibleMoves = neighbors(inputArr, visited, position);
 
     for (let i = 0; i < possibleMoves.length; i++) {
       const move = possibleMoves[i];
 
-      if (!visited.includes(`${move[0]},${move[1]}`)) {
-        queue.unshift(move);
+      const coordinates = `${move[0]},${move[1]}`;
+      visited.push(coordinates);
 
-        if (pathTaken[move[0]]) {
-          pathTaken[move[0]][move[1]] = position;
-        } else {
-          pathTaken[move[0]] = [];
-          pathTaken[move[0]][move[1]] = position;
+      queue.unshift(move);
+
+      if (pathTaken[move[0]]) {
+        pathTaken[move[0]][move[1]] = position;
+      } else {
+        pathTaken[move[0]] = [];
+        pathTaken[move[0]][move[1]] = position;
+      }
+
+      if (`${move[0]},${move[1]}` === `${to[0]},${to[1]}`) {
+        let nextStep = pathTaken[to[0]][to[1]];
+
+        let steps = 0;
+
+        while (nextStep && nextStep !== from) {
+          nextStep = pathTaken[nextStep[0]][nextStep[1]];
+          steps++;
         }
 
-        if (`${move[0]},${move[1]}` === `${to[0]},${to[1]}`) {
-          let nextStep = pathTaken[to[0]][to[1]];
-
-          let steps = 0;
-
-          while (nextStep) {
-            nextStep = pathTaken[nextStep[0]]?.[nextStep[1]];
-            steps++;
-          }
-
-          return steps;
-        }
+        return steps + 1;
       }
     }
   }
   return -1;
 };
-
-//problematic step
-console.log(bfs(inputCoordinates['0'], inputCoordinates['2']));
 
 const distances: { [key: string]: number } = {};
 
@@ -89,8 +82,6 @@ let right = 1;
 while (left < locations.length - 1) {
   const from = locations[left];
   const to = locations[right];
-
-  console.log(left, right);
 
   const distance = bfs(inputCoordinates[from], inputCoordinates[to]);
 
--- 2016/24/helpers/neighbors.ts
+++ 2016/24/helpers/neighbors.ts
@@ -1,20 +1,40 @@
-const neighbors = (arr: string[], position: [number, number]) => {
+const neighbors = (
+  arr: string[],
+  visited: string[],
+  position: [number, number]
+) => {
   const validMoves: [number, number][] = [];
   const [x, y] = position;
 
-  if (x - 1 > 0 && arr[y][x - 1] !== '#') {
+  if (
+    x - 1 > 0 &&
+    arr[y][x - 1] !== '#' &&
+    !visited.includes(`${x - 1},${y}`)
+  ) {
     validMoves.push([x - 1, y]);
   }
 
-  if (x + 1 < arr[0].length && arr[y][x + 1] !== '#') {
+  if (
+    x + 1 < arr[0].length &&
+    arr[y][x + 1] !== '#' &&
+    !visited.includes(`${x + 1},${y}`)
+  ) {
     validMoves.push([x + 1, y]);
   }
 
-  if (y - 1 > 0 && arr[y - 1][x] !== '#') {
+  if (
+    y - 1 > 0 &&
+    arr[y - 1][x] !== '#' &&
+    !visited.includes(`${x},${y - 1}`)
+  ) {
     validMoves.push([x, y - 1]);
   }
 
-  if (y + 1 < arr.length - 1 && arr[y + 1][x] !== '#') {
+  if (
+    y + 1 < arr.length - 1 &&
+    arr[y + 1][x] !== '#' &&
+    !visited.includes(`${x},${y + 1}`)
+  ) {
     validMoves.push([x, y + 1]);
   }
 
