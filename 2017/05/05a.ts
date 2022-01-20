432e2a891f57e87598fb82cb17ba1f3446ddef9b
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-20 04:53:54 +0100

    2017 day 5 part 1

---
+++ 2017/05/05a.ts
@@ -0,0 +1,21 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+console.log(inputArr);
+
+let currIdx = 0;
+let stepsTaken = 0;
+
+while (currIdx < inputArr.length && currIdx >= 0) {
+  const move = inputArr[currIdx];
+
+  inputArr[currIdx]++;
+
+  currIdx += move;
+  stepsTaken++;
+}
+
+console.log(stepsTaken);
