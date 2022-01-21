986f9eb13fb1d943a5a86398e526fb05f89393ca
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-21 12:28:46 +0100

    2017 day8 part 2

---
+++ 2017/08/08b.ts
@@ -0,0 +1,69 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+const createRegisters = (arr: string[][]) => {
+  const registers: { [key: string]: number } = {};
+
+  arr.forEach((line) => {
+    if (!registers[line[1]]) {
+      registers[line[1]] = 0;
+    }
+
+    if (!registers[line[3]]) {
+      registers[line[3]] = 0;
+    }
+  });
+
+  return registers;
+};
+
+const executeInstruction = (instruction: string[]) => {
+  const [op, target, val, conTarget, con, conVal] = instruction;
+
+  const modifier = op === 'inc' ? 1 : -1;
+
+  if (con === '>') {
+    if (registers[conTarget] > +conVal) {
+      registers[target] += +val * modifier;
+    }
+  }
+
+  if (con === '<')
+    if (registers[conTarget] < +conVal) {
+      registers[target] += +val * modifier;
+    }
+
+  if (con === '<=')
+    if (registers[conTarget] <= +conVal) {
+      registers[target] += +val * modifier;
+    }
+
+  if (con === '>=')
+    if (registers[conTarget] >= +conVal) {
+      registers[target] += +val * modifier;
+    }
+
+  if (con === '==')
+    if (registers[conTarget] === +conVal) {
+      registers[target] += +val * modifier;
+    }
+
+  if (con === '!=')
+    if (registers[conTarget] !== +conVal) {
+      registers[target] += +val * modifier;
+    }
+};
+
+const registers = createRegisters(inputArr);
+
+let maxEver = 0;
+
+inputArr.forEach((instruction) => {
+  executeInstruction(instruction);
+  maxEver = Math.max(...Object.values(registers), maxEver);
+});
+
+console.log(maxEver);
