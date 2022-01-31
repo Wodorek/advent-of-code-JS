eca18da2da458a5652bf7ad00544dabdcec71260
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-01-31 20:11:43 +0100

    Update prepareInput.ts

--- 2018/03/helpers/prepareInput.ts
+++ 2018/03/helpers/prepareInput.ts
@@ -1,7 +1,8 @@
-const prepareInput = (input: string) => {
+const prepareInput = (input: string): [number, number[], number[]][] => {
   return input.split('\n').map((el) => {
     const split = el.split(' ');
     return [
+      +split[0].slice(1),
       [
         ...split[2].split(',').map((el) => {
           return +el.replace(':', '');
