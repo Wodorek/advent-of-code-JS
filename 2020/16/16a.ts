152770a14ef3fb4022ddaefab2272e312790bab2
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-02-24 14:38:42 +0100

    2020 day 16 part 1

---
+++ 2020/16/16a.ts
@@ -0,0 +1,27 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const [fields, myTicket, nearbyTickets] = prepareInput(input);
+
+const possibleFields = new Set<number>();
+
+fields.forEach((field) => {
+  for (let i = field[1][0]; i < field[1][1] + 1; i++) {
+    possibleFields.add(i);
+  }
+
+  for (let i = field[2][0]; i < field[2][1] + 1; i++) {
+    possibleFields.add(i);
+  }
+});
+
+let errorRate = 0;
+
+nearbyTickets.forEach((ticket) => {
+  if (!possibleFields.has(ticket)) {
+    errorRate += ticket;
+  }
+});
+
+console.log(errorRate);
---
+++ 2020/16/helpers/prepareInput.ts
@@ -0,0 +1,33 @@
+const prepareInput = (input: string) => {
+  const split = input.split('\n');
+
+  const firstSeparator = split.indexOf('');
+  const secondSeparator = split.indexOf('', firstSeparator + 1);
+
+  const fields = split.slice(0, firstSeparator).map((el) => {
+    const split = el.split(' ');
+    return [
+      split[0].replace(':', ''),
+      split[1].split('-').map(Number),
+      split[3].split('-').map(Number),
+    ];
+  });
+
+  const myTicket = split.slice(firstSeparator + 1, secondSeparator);
+
+  const nearbyTickets = [];
+
+  for (let i = secondSeparator + 2; i < split.length; i++) {
+    const el = split[i];
+
+    nearbyTickets.push(...el.split(',').map(Number));
+  }
+
+  return [fields, myTicket[1].split(',').map(Number), nearbyTickets] as [
+    [string, number[], number[]][],
+    number[],
+    number[]
+  ];
+};
+
+export default prepareInput;
