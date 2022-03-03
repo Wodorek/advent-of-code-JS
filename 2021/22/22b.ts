bf440457e3522bf1bc3997e0c357758f1c8316a5
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-03-03 16:19:24 +0100

    2021 WIP
    Not working, will be changed

---
+++ 2021/22/22b.ts
@@ -0,0 +1,10 @@
+import input from './input';
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+const cubeBounds = [
+  [0, 0],
+  [0, 0],
+  [0, 0],
+];
