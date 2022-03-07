c30392bdd0eeed5126f437b3366fe5a878cf70b2
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-03-07 12:53:02 +0100

    2021 day 18 reset

--- 2021/18/18a.ts
+++
@@ -1,6 +0,0 @@
-import input from './input';
-import prepareInput from './helpers/prepareInput';
-
-const inputArr = prepareInput(input);
-
-//placeholder for now...
--- 2021/18/helpers/getArrayDepth.ts
+++
@@ -1,5 +0,0 @@
-const getArrayDepth = (value: any): number => {
-  return Array.isArray(value) ? 1 + Math.max(...value.map(getArrayDepth)) : 0;
-};
-
-export default getArrayDepth;
