b9a70f0a64a94fa270f5f576ce851ca6cc1b8b6b
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-03-14 16:04:46 +0100

    2015 day 5 part 1

--- 2019/05/05a.ts
+++ 2019/05/05a.ts
@@ -6,9 +6,13 @@ const inputArr = prepareInput(input);
 
 let currPos = 0;
 
+const memory: { currPos: number; opcode: number; params: number[] }[] = [];
+
 const executeCode = (list: number[], input: number) => {
   const asStr = list[currPos].toString().padStart(5, '0');
   const opcode = parseInt(asStr.slice(3));
+
+  console.log(opcode);
 
   if (isNaN(opcode)) {
     console.log('WARN', opcode);
@@ -16,25 +20,29 @@ const executeCode = (list: number[], input: number) => {
 
   const param1 = asStr[2] === '0' ? list[currPos + 1] : currPos + 1;
   const param2 = asStr[1] === '0' ? list[currPos + 2] : currPos + 2;
-  const param3 = asStr[0] === '0' ? list[currPos + 3] : currPos + 3;
+  const writeTo = asStr[0] === '0' ? list[currPos + 3] : currPos + 3;
 
+  memory.push({ currPos, opcode, params: [param1, param2, writeTo] });
   if (opcode === 1) {
-    list[param3] = list[param1] + list[param2];
+    list[writeTo] = list[param1] + list[param2];
     currPos += 4;
   }
 
   if (opcode === 2) {
-    list[param3] = list[param1] * list[param2];
+    list[writeTo] = list[param1] * list[param2];
     currPos += 4;
   }
 
   if (opcode === 3) {
-    list[param3] = input;
+    list[writeTo] = input;
     currPos += 2;
   }
 
   if (opcode === 4) {
-    console.log(list[param3]);
+    if (list[writeTo] !== 0) {
+      console.log(list[writeTo]);
+    }
+
     currPos += 2;
   }
 
@@ -49,7 +57,12 @@ const executeCode = (list: number[], input: number) => {
 
 let shoudContinue = true;
 
-console.log(inputArr);
 while (shoudContinue) {
   shoudContinue = executeCode(inputArr, 1);
 }
+
+console.log(
+  memory[memory.length - 1].params.filter((el) => {
+    return el > 1000;
+  })[0]
+);
