1149cf1d5f1da57bdb8e2995c735d7a696d6dd19
Wodorek <Krol.w94@gmail.com>
2022-04-16 15:21:39 +0200

    2019 day 07 part 1

--- 2019/07/07a.ts
+++ 2019/07/07a.ts
@@ -1,7 +1,48 @@
 import input from './input';
 
 import prepareInput from './helpers/prepareInput';
+import VM from '../computer/VM';
+import permute from './helpers/permute';
 
 const inputArr = prepareInput(input);
 
-console.log(inputArr);
+const phases = [0, 1, 2, 3, 4];
+
+const permutations = permute(phases);
+
+const test = [1, 0, 4, 3, 2];
+
+const outputs: number[] = [];
+
+test.forEach((input, idx) => {
+  const vm = new VM(inputArr);
+
+  let shouldContinue = true;
+  const inputs = [input, outputs[idx - 1] || 0];
+
+  while (shouldContinue) {
+    shouldContinue = vm.exectuteOperation(inputs, outputs);
+  }
+});
+
+const maxOutputs: number[] = [];
+
+permutations.forEach((permutation) => {
+  const outputs: number[] = [];
+
+  permutation.forEach((phase, idx) => {
+    const inputs = [phase, outputs[idx - 1] || 0];
+
+    let shouldContinue = true;
+
+    const vm = new VM(inputArr);
+
+    while (shouldContinue) {
+      shouldContinue = vm.exectuteOperation(inputs, outputs);
+    }
+  });
+
+  maxOutputs.push(outputs[outputs.length - 1]);
+});
+
+console.log(Math.max(...maxOutputs));
