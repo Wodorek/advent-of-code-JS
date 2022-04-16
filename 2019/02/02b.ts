bc66eecb2dcd53b50419cb898bdbc68d768a933e
Wodorek <Krol.w94@gmail.com>
2022-04-16 18:29:32 +0200

    2019 day 2 part 2
    Redone the solution using new intocde computer implementation

--- 2019/02/02b.ts
+++ 2019/02/02b.ts
@@ -1,50 +1,33 @@
 import input from './input';
 
 import prepareInput from './helpers/prepareInput';
+import { VM } from '../computer/VM';
 
 const inputArr = prepareInput(input);
 
-const executeCode = (pos: number, list: number[]) => {
-  const opcode = list[pos];
+console.log(inputArr);
 
-  if (opcode === 1) {
-    list[list[pos + 3]] = list[list[pos + 1]] + list[list[pos + 2]];
+function testNounVerb(noun: number, verb: number) {
+  const memory = inputArr.map((el) => el);
+  memory[1] = noun;
+  memory[2] = verb;
+
+  const vm = new VM(memory);
+
+  while (vm.working) {
+    vm.executeInstruction();
   }
 
-  if (opcode === 2) {
-    list[list[pos + 3]] = list[list[pos + 1]] * list[list[pos + 2]];
-  }
-
-  if (opcode === 99) {
-    return false;
-  }
-
-  return true;
-};
-
-const runProgram = (noun: number, verb: number) => {
-  const list = [...inputArr];
-
-  let shoudContinue = true;
-
-  list[1] = noun;
-  list[2] = verb;
-
-  let currPos = 0;
-
-  while (shoudContinue) {
-    shoudContinue = executeCode(currPos, list);
-    currPos += 4;
-  }
-
-  return list[0];
-};
+  return vm.memory[0];
+}
 
 for (let noun = 0; noun < 99; noun++) {
   for (let verb = 0; verb < 99; verb++) {
-    const value = runProgram(noun, verb);
+    const value = testNounVerb(noun, verb);
+
     if (value === 19690720) {
       console.log('solution:', 100 * noun + verb);
+      break;
     }
   }
 }
