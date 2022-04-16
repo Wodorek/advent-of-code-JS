f561a82f00b4f68c6727fc14449b62f733070493
Wodorek <Krol.w94@gmail.com>
2022-04-16 21:10:38 +0200

    2019 day 5 part 1
    Rewritten using intocde computer

--- 2019/05/05a.ts
+++ 2019/05/05a.ts
@@ -1,53 +1,14 @@
 import input from './input';
 
 import prepareInput from './helpers/prepareInput';
+import { VM } from '../computer/VM';
 
 const inputArr = prepareInput(input);
 
-let currPos = 0;
+const vm = new VM(inputArr);
 
-const executeCode = (list: number[], input: number) => {
-  const asStr = list[currPos].toString().padStart(5, '0');
-  const opcode = parseInt(asStr.slice(3));
-
-  if (isNaN(opcode)) {
-    console.log('WARN', opcode);
-  }
-
-  const param1 = asStr[2] === '0' ? list[currPos + 1] : currPos + 1;
-  const param2 = asStr[1] === '0' ? list[currPos + 2] : currPos + 2;
-  const writeTo = asStr[0] === '0' ? list[currPos + 3] : currPos + 3;
-
-  if (opcode === 1) {
-    list[writeTo] = list[param1] + list[param2];
-    currPos += 4;
-  }
-
-  if (opcode === 2) {
-    list[writeTo] = list[param1] * list[param2];
-    currPos += 4;
-  }
-
-  if (opcode === 3) {
-    list[writeTo] = input;
-    currPos += 2;
-  }
-
-  if (opcode === 4) {
-    console.log(list[param1]);
-
-    currPos += 2;
-  }
-
-  if (opcode === 99) {
-    return false;
-  }
-
-  return true;
-};
-
-let shoudContinue = true;
-
-while (shoudContinue) {
-  shoudContinue = executeCode(inputArr, 1);
+while (vm.working) {
+  vm.executeInstruction(1);
 }
+
+console.log(vm.outputs[vm.outputs.length - 1]);
--- 2019/computer/VM.ts
+++ 2019/computer/VM.ts
@@ -2,11 +2,13 @@ export class VM {
   memory: number[];
   pointer: number;
   working: boolean;
+  outputs: number[];
 
   constructor(memory: number[]) {
     this.memory = memory;
     this.pointer = 0;
     this.working = true;
+    this.outputs = [];
   }
 
   ops = {
@@ -22,19 +24,68 @@ export class VM {
       this.pointer += 4;
     },
 
+    3: (p1: number, p2: number, p3: number, input: number) => {
+      this.memory[p1] = input;
+
+      this.pointer += 2;
+    },
+
+    4: (p1: number) => {
+      this.outputs.push(this.memory[p1]);
+
+      this.pointer += 2;
+    },
+
+    5: (p1: number, p2: number) => {
+      if (this.memory[p1] !== 0) {
+        this.pointer = this.memory[p2];
+      } else {
+        this.pointer += 3;
+      }
+    },
+
+    6: (p1: number, p2: number) => {
+      if (this.memory[p1] === 0) {
+        this.pointer = this.memory[p2];
+      } else {
+        this.pointer += 3;
+      }
+    },
+
+    7: (p1: number, p2: number, p3: number) => {
+      if (this.memory[p1] < this.memory[p2]) {
+        this.memory[p3] = 1;
+      } else {
+        this.memory[p3] = 0;
+      }
+      this.pointer += 4;
+    },
+
+    8: (p1: number, p2: number, p3: number) => {
+      if (this.memory[p1] === this.memory[p2]) {
+        this.memory[p3] = 1;
+      } else {
+        this.memory[p3] = 0;
+      }
+      this.pointer += 4;
+    },
+
     99: () => {
       this.working = false;
     },
   };
 
-  executeInstruction() {
+  executeInstruction(input = 0) {
     const asStr = this.memory[this.pointer].toString().padStart(5, '0');
     const opcode = parseInt(asStr.slice(3)) as keyof typeof this.ops;
 
-    this.ops[opcode](
-      this.memory[this.pointer + 1],
-      this.memory[this.pointer + 2],
-      this.memory[this.pointer + 3]
-    );
+    const param1 =
+      asStr[2] === '0' ? this.memory[this.pointer + 1] : this.pointer + 1;
+    const param2 =
+      asStr[1] === '0' ? this.memory[this.pointer + 2] : this.pointer + 2;
+    const param3 =
+      asStr[0] === '0' ? this.memory[this.pointer + 3] : this.pointer + 3;
+
+    this.ops[opcode](param1, param2, param3, input);
   }
 }
