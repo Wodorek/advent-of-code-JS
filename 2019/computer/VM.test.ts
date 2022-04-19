88095bea5d6df180be3ef39ed375f07c57a87285
Wodorek <Krol.w94@gmail.com>
2022-04-20 00:55:52 +0200

    VM test update

--- 2019/computer/VM.test.ts
+++ 2019/computer/VM.test.ts
@@ -127,4 +127,83 @@ describe('Works for day 7', () => {
 
     expect(Math.max(...maxOutputs)).toBe(398674);
   });
+
+  it('works for part 2', () => {
+    const phases = [5, 6, 7, 8, 9];
+
+    const permutations = permute(phases);
+
+    let maxThrust = -Infinity;
+
+    permutations.forEach((permutation) => {
+      const machines = {
+        vmA: {
+          machine: new VM(input),
+          inputs: [permutation[0], 0],
+        },
+        vmB: {
+          machine: new VM(input),
+          inputs: [permutation[1]],
+        },
+        vmC: {
+          machine: new VM(input),
+          inputs: [permutation[2]],
+        },
+        vmD: {
+          machine: new VM(input),
+          inputs: [permutation[3]],
+        },
+        vmE: {
+          machine: new VM(input),
+          inputs: [permutation[4]],
+        },
+      };
+
+      type MachineKey = keyof typeof machines;
+
+      let currMachine: MachineKey = 'vmA';
+
+      function advanceMachine(currMachine: MachineKey): MachineKey {
+        const machinesOrder: MachineKey[] = ['vmA', 'vmB', 'vmC', 'vmD', 'vmE'];
+
+        const machineIndex = machinesOrder.indexOf(currMachine);
+
+        if (machineIndex === 4) {
+          return machinesOrder[0];
+        } else {
+          return machinesOrder[machineIndex + 1];
+        }
+      }
+
+      while (true) {
+        const workingMachine = machines[currMachine];
+
+        workingMachine.machine.executeInstruction(
+          workingMachine.inputs[workingMachine.machine.inputIdx]
+        );
+
+        if (workingMachine.machine.lastCommand === 3) {
+          workingMachine.machine.inputIdx++;
+        }
+
+        if (workingMachine.machine.lastCommand === 4) {
+          machines[advanceMachine(currMachine)].inputs.push(
+            workingMachine.machine.getLastOutput
+          );
+          currMachine = advanceMachine(currMachine);
+        }
+
+        if (!workingMachine.machine.working) {
+          break;
+        }
+      }
+
+      maxThrust = Math.max(
+        maxThrust,
+        machines[currMachine].inputs[machines[currMachine].inputs.length - 1]
+      );
+    });
+
+    expect(maxThrust).toBe(39431233);
+  });
 });
