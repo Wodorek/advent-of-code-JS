a7747827ab9806b5b52331bcc403da6ee1dd8358
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-04-26 19:41:14 +0200

    2018 day 19 part 2

---
+++ 2018/19/19b.ts
@@ -0,0 +1,191 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const [bindTo, instructions] = prepareInput(input);
+
+class OpcodeDevice {
+  registers: number[];
+  working: boolean;
+  pointerValue: number;
+  pointerBoundTo: number;
+
+  constructor(initialRegisters: number[], pointer: number) {
+    this.registers = initialRegisters;
+    this.working = true;
+    this.pointerBoundTo = pointer;
+    this.pointerValue = this.registers[pointer];
+  }
+
+  bindPointer(to: number) {
+    this.pointerValue = this.registers[to];
+  }
+
+  ops = {
+    addr: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] + this.registers[B];
+
+      return modifiedRegisters;
+    },
+    addi: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] + B;
+
+      return modifiedRegisters;
+    },
+    mulr: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] * this.registers[B];
+
+      return modifiedRegisters;
+    },
+    muli: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] * B;
+
+      return modifiedRegisters;
+    },
+    banr: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] & this.registers[B];
+
+      return modifiedRegisters;
+    },
+    bani: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] & B;
+
+      return modifiedRegisters;
+    },
+    boor: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] | this.registers[B];
+
+      return modifiedRegisters;
+    },
+    bori: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] | B;
+
+      return modifiedRegisters;
+    },
+    setr: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A];
+
+      return modifiedRegisters;
+    },
+    seti: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = A;
+
+      return modifiedRegisters;
+    },
+    gtir: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = A > this.registers[B] ? 1 : 0;
+
+      return modifiedRegisters;
+    },
+    gtri: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] > B ? 1 : 0;
+
+      return modifiedRegisters;
+    },
+    gtrr: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] > this.registers[B] ? 1 : 0;
+
+      return modifiedRegisters;
+    },
+    eqir: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = A === this.registers[B] ? 1 : 0;
+
+      return modifiedRegisters;
+    },
+    eqri: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] === B ? 1 : 0;
+
+      return modifiedRegisters;
+    },
+    eqrr: (A: number, B: number, C: number) => {
+      const modifiedRegisters = [...this.registers];
+
+      modifiedRegisters[C] = this.registers[A] === this.registers[B] ? 1 : 0;
+
+      return modifiedRegisters;
+    },
+  };
+
+  executeInstruction(operation: string, A: number, B: number, C: number) {
+    if (this.pointerValue) {
+      this.registers[this.pointerBoundTo] = this.pointerValue;
+    }
+
+    const modifiedRegisters = this.ops[operation as keyof typeof this.ops](
+      A,
+      B,
+      C
+    );
+    this.registers = modifiedRegisters;
+    this.pointerValue = this.registers[this.pointerBoundTo];
+    this.pointerValue++;
+
+    return this.pointerValue;
+  }
+}
+
+let maxRegValue = 0;
+
+let totalLoops = 0;
+
+const device = new OpcodeDevice([1, 0, 0, 0, 0, 0], bindTo);
+
+let currentInst = device.registers[bindTo];
+
+while (device.working) {
+  if (currentInst > instructions.length - 1) {
+    break;
+  }
+
+  if (totalLoops > 1000) {
+    break;
+  }
+
+  const registers = device.registers;
+
+  registers.forEach((reg, idx) => {
+    maxRegValue = Math.max(maxRegValue, reg);
+  });
+  currentInst = device.executeInstruction(...instructions[currentInst]);
+  totalLoops++;
+}
+
+function calculateSum(num: any) {
+  const arr: any = [...new Array(num + 1).keys()]
+    .slice(1)
+    .reduce((s: any, a: any) => s + (!(num % a) && a), 0);
+
+  return arr;
+}
+
+console.log(calculateSum(maxRegValue));
