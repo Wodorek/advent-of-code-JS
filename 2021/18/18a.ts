634d4edce78373bae0ca10a9dd8d3c32b1bb98a3
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-04-27 20:08:39 +0200

    Almost there?

--- 2021/18/18a.ts
+++ 2021/18/18a.ts
@@ -9,27 +9,30 @@ class SnailNum {
   right: number | SnailNum;
   depth: number;
   prev: SnailNum | null;
+  position: 'left' | 'right' | 'root';
 
   constructor(
     left: number | SnailNum | any[],
     right: number | SnailNum | any[],
     depth: number,
-    prev: SnailNum | null
+    prev: SnailNum | null,
+    position: 'left' | 'right' | 'root'
   ) {
     if (typeof left === 'number' || left instanceof SnailNum) {
       this.left = left;
     } else {
-      this.left = new SnailNum(left[0], left[1], depth + 1, this);
+      this.left = new SnailNum(left[0], left[1], depth + 1, this, 'left');
     }
 
     if (typeof right === 'number' || right instanceof SnailNum) {
       this.right = right;
     } else {
-      this.right = new SnailNum(right[0], right[1], depth + 1, this);
+      this.right = new SnailNum(right[0], right[1], depth + 1, this, 'right');
     }
 
     this.depth = depth;
     this.prev = prev;
+    this.position = position;
   }
 
   turnIntoArr(): any[] {
@@ -49,17 +52,17 @@ class SnailNum {
   }
 
   add(num: SnailNum) {
-    const newNum = new SnailNum(this, num, 0, null);
+    const newNum = new SnailNum(this, num, 0, null, 'root');
 
     const asArr = newNum.turnIntoArr();
 
-    return new SnailNum(asArr[0], asArr[1], 0, null);
+    return new SnailNum(asArr[0], asArr[1], 0, null, 'root');
   }
 
   explode() {
     const queue = [this.left, this.right];
 
-    const candidates = [];
+    const candidates: SnailNum[] = [];
 
     while (queue.length > 0) {
       const toCheck = queue.pop()!;
@@ -73,12 +76,68 @@ class SnailNum {
       }
     }
 
-    console.log(candidates);
+    let toExplode: SnailNum | number = candidates[candidates.length - 1];
+
+    console.log(toExplode.prev);
+
+    let left = toExplode.left;
+    let right = toExplode.right;
+
+    let rightLookup = toExplode.prev;
+    let leftLookup = toExplode.prev;
+
+    while (true) {
+      if (typeof rightLookup?.right === 'number') {
+        rightLookup.right += right as number;
+        break;
+      } else if (rightLookup?.prev === null) {
+        break;
+      } else {
+        rightLookup = rightLookup!.prev;
+      }
+    }
+
+    while (true) {
+      if (typeof leftLookup?.left === 'number') {
+        leftLookup.left += left as number;
+        break;
+      } else if (leftLookup?.prev === null) {
+        break;
+      } else {
+        leftLookup = leftLookup!.prev;
+      }
+    }
+
+    if (toExplode.position === 'left') {
+      toExplode!.prev!.left = 0;
+    }
+
+    if (toExplode.position === 'right') {
+      toExplode!.prev!.right = 0;
+    }
   }
 }
 
-const arr1 = [[[[[9, 8], 1], 2], 3], 4];
+const arr1: any[] = [
+  [3, [2, [1, [7, 3]]]],
+  [6, [5, [4, [3, 2]]]],
+];
 
-const snailNum = new SnailNum(arr1[0], arr1[1], 0, null);
+const snailNum = new SnailNum(arr1[0], arr1[1], 0, null, 'root');
+
+console.log(`${JSON.stringify(snailNum.turnIntoArr())}`);
 
 snailNum.explode();
+
+console.log(`${JSON.stringify(snailNum.turnIntoArr())}`);
+
+console.log(
+  `[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]` ===
+    `${JSON.stringify(snailNum.turnIntoArr())}`
+);
+// `[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]`
+
+[
+  [3, [2, [1, [7, 3]]]],
+  [6, [5, [4, [3, 2]]]],
+];
