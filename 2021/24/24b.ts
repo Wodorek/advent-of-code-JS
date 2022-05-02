55ce566733cbf618fe19775a770af084c0493879
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-05-02 15:34:54 +0200

    2021 day 24 part  2

---
+++ 2021/24/24b.ts
@@ -0,0 +1,90 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+import { ALU } from './ALU';
+
+const inputArr = prepareInput(input);
+
+//74929995999389
+
+let num = 11118151637112;
+const vars: any[] = [];
+
+const alu = new ALU(num, inputArr);
+
+const isValid = alu.runProgram();
+
+console.log(alu.vars);
+//  inp w
+//   mul x 0
+//   add x z
+//   mod x 26
+//   div z 26 this
+//   add x 11 this c x
+//   eql x w
+//   eql x 0
+//   mul y 0
+//   add y 25
+//   mul y x
+//   add y 1
+//   mul z y
+//   mul y 0
+//   add y w
+//   add y 14 this o y
+//   mul y x
+//   add z y
+
+//PUSH 11,14
+//PUSH 13,8
+//PUSH 11,4
+//PUSH 10,10
+//POP -3,14
+//POP -4,10
+//PUSH 12,4
+//POP -8,14
+//POP -3,1
+//POP -12,6
+//PUSH 14,0
+//POP -6,9
+//PUSH 11,13
+//POP -12,12
+
+// PUSH input[0] + 14
+// PUSH input[1] + 8
+// PUSH input[2] + 4-
+// PUSH input[3] + 10-
+// POP input[4] === popped -3
+// POP input[5] === popped -4
+// PUSH input[6] + 4 -
+// POP input[7] === popped -8
+// POP input[8] === popped -3
+// POP input[9] === popped -12
+// PUSH input[10] + 0
+// POP input[11] === popped -6
+// PUSH input[12] + 13
+// POP input[13] === popped -12
+
+// input[4] === input[3] + 7
+// input[5] === input[2]
+// input[7] === input[6] - 4
+// input[8] === input[1] + 5
+// input[9] === input[0] +2
+// input[11] == input[10] -6
+// input[13] === input[12] +1
+
+// 0 = 1
+// 1 = 1
+// 2 = 1
+// 3 = 1
+// 4 = 8
+// 5 = 1
+// 6 = 5
+// 7 = 1
+// 8 = 6
+// 9 = 3
+// 10 = 7
+// 11 = 1
+// 12 = 1
+//13 = 2
+
+const solution = 11118151637112;
