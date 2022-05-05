986eb9925d7aeb6a40535d6dd333b72d721383f6
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-05-05 19:52:59 +0200

    Year 2021 complete!

---
+++ 2021/25/25b.ts
@@ -0,0 +1,11 @@
+// You use all fifty stars to boost the signal and remotely start the sleigh! Now, you just have to find your way back to the surface...
+
+// ...did you know crab submarines come with colored lights?
+
+// Congratulations! You've finished every puzzle in Advent of Code 2021! I hope you had as much fun solving them as I had making them for you. I'd love to hear about your adventure; you can get in touch with me via contact info on my website or through Twitter.
+
+// If you'd like to see more things like this in the future, please consider supporting Advent of Code and sharing it with others.
+
+// To hear about future projects, you can follow me on Twitter.
+
+// I've highlighted the easter eggs in each puzzle, just in case you missed any. Hover your mouse over them, and the easter egg will appear.
