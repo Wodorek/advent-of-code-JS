5b2fdf80736b0350942b67be7d83cdf2bf42c01b
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-05-13 10:27:22 +0200

    2020 day 18 part 2

---
+++ 2020/18/18b.ts
@@ -0,0 +1,74 @@
+// Full credit to that page
+// https://en.wikipedia.org/wiki/Shunting_yard_algorithm#:~:text=In%20computer%20science%2C%20the%20shunting,abstract%20syntax%20tree%20(AST).
+
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+
+const inputArr = prepareInput(input);
+
+function weirdSum2(str: string) {
+  const split = str.split('').filter((el) => {
+    return el !== ' ';
+  });
+
+  const RPN: string[] = [];
+  const operators: string[] = [];
+
+  const precedence = {
+    '+': 1,
+    '*': 0,
+  };
+
+  for (let i = 0; i < split.length; i++) {
+    if (!isNaN(+split[i])) {
+      RPN.push(split[i]);
+    } else if (split[i] === '+' || split[i] === '*') {
+      while (
+        operators.length !== 0 &&
+        precedence[
+          operators[operators.length - 1] as keyof typeof precedence
+        ] >= precedence[split[i] as keyof typeof precedence] &&
+        !(operators[operators.length - 1] === '(')
+      ) {
+        RPN.push(operators.pop()!);
+      }
+      operators.push(split[i]);
+    } else if (split[i] === '(') {
+      operators.push(split[i]);
+    } else if (split[i] === ')') {
+      while (operators[operators.length - 1] !== '(') {
+        RPN.push(operators.pop()!);
+      }
+      if (operators[operators.length - 1] === '(') {
+        operators.pop();
+      }
+    }
+  }
+
+  while (operators.length !== 0) {
+    RPN.push(operators.pop()!);
+  }
+
+  const finalStack: string[] = [];
+
+  RPN.forEach((el) => {
+    if (!isNaN(+el)) {
+      finalStack.push(el);
+    } else {
+      const right = finalStack.pop()!;
+      const left = finalStack.pop()!;
+      finalStack.push(eval(`${left} ${el} ${right}`));
+    }
+  });
+
+  return finalStack[0];
+}
+
+let total = 0;
+
+inputArr.forEach((num) => {
+  total += +weirdSum2(num);
+});
+
+console.log(total);
