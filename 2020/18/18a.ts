a3da2521b61de1bc76a08e438937c176ae8cae03
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-05-13 10:26:05 +0200

    2020 day 18 part 1

--- 2020/18/18a.ts
+++ 2020/18/18a.ts
@@ -1,28 +1,66 @@
+// Full credit to that page
+// https://en.wikipedia.org/wiki/Shunting_yard_algorithm#:~:text=In%20computer%20science%2C%20the%20shunting,abstract%20syntax%20tree%20(AST).
+
 import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
 const inputArr = prepareInput(input);
 
-function calculate(numString: string) {
-  const operations: { [key: number]: '+' | '*' } = {};
+function weirdSum(str: string) {
+  const split = str.split('').filter((el) => {
+    return el !== ' ';
+  });
 
-  let currOp = 0;
+  const RPN: string[] = [];
+  const operators: string[] = [];
 
-  const split = numString.split('');
+  for (let i = 0; i < split.length; i++) {
+    if (!isNaN(+split[i])) {
+      RPN.push(split[i]);
+    } else if (split[i] === '+' || split[i] === '*') {
+      while (
+        operators.length !== 0 &&
+        !(operators[operators.length - 1] === '(')
+      ) {
+        RPN.push(operators.pop()!);
+      }
+      operators.push(split[i]);
+    } else if (split[i] === '(') {
+      operators.push(split[i]);
+    } else if (split[i] === ')') {
+      while (operators[operators.length - 1] !== '(') {
+        RPN.push(operators.pop()!);
+      }
+      if (operators[operators.length - 1] === '(') {
+        operators.pop();
+      }
+    }
+  }
 
-  numString.split('').forEach((el) => {
-    if (el === '+' || el === '*') {
-      operations[currOp] = el;
-      currOp++;
+  while (operators.length !== 0) {
+    RPN.push(operators.pop()!);
+  }
+
+  const finalStack: string[] = [];
+
+  RPN.forEach((el) => {
+    if (!isNaN(+el)) {
+      finalStack.push(el);
+    } else {
+      const right = finalStack.pop()!;
+      const left = finalStack.pop()!;
+      finalStack.push(eval(`${left} ${el} ${right}`));
     }
   });
 
-  console.log(
-    split.filter((el) => {
-      return el !== ' ';
-    })
-  );
+  return finalStack[0];
 }
 
-calculate(`2 * 3 + (4 * 5)`);
+let total = 0;
+
+inputArr.forEach((num) => {
+  total += +weirdSum(num);
+});
+
+console.log(total);
