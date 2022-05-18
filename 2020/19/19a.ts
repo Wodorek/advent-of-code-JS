b78395d9edec39d26cdc809beea009c13bc49482
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-05-18 13:34:01 +0200

    2020 day 19 part 1

--- 2020/19/19a.ts
+++ 2020/19/19a.ts
@@ -15,10 +15,7 @@ rules.forEach((rule) => {
   const subRules = split[1].split(' ').slice(1);
   let currSet: number[] = [];
 
-  console.log(subRules[0]);
-
   if (subRules[0] === '"a"' || subRules[0] === '"b"') {
-    console.log('he');
     basicBlocks[id] = eval(subRules[0]);
     return;
   }
@@ -37,10 +34,65 @@ rules.forEach((rule) => {
   parsedRules[id].push(currSet);
 });
 
-console.log(parsedRules);
-console.log(eval(`${parsedRules[1]}`));
-console.log(basicBlocks);
+function getPossibleStrings(id: number) {
+  const queue = [...parsedRules[id]];
+  const outputs: number[][] = [];
 
-function destructureRule(id: number) {
-  const rule = parsedRules[id];
+  while (queue.length > 0) {
+    const item = queue.pop()!;
+
+    let changed = false;
+
+    for (let i = 0; i < item.length; i++) {
+      if (parsedRules[item[i]]) {
+        if (parsedRules[item[i]].length > 1) {
+          const item2 = item.map((el) => {
+            return el;
+          });
+
+          item.splice(i, 1, ...parsedRules[item[i]][0]);
+          item2.splice(i, 1, ...parsedRules[item2[i]][1]);
+
+          changed = true;
+          queue.push(item, item2);
+          break;
+        } else {
+          item.splice(i, 1, ...parsedRules[item[i]][0]);
+          changed = true;
+          queue.push(item);
+          break;
+        }
+      }
+    }
+
+    if (!changed) {
+      outputs.push(item);
+    }
+  }
+
+  return outputs;
 }
+
+function parseIntoStrings(codes: number[][]) {
+  return codes.map((code) => {
+    return code
+      .map((el) => {
+        return basicBlocks[el];
+      })
+      .join('');
+  });
+}
+
+function countMatching(possible: string[], messages: string[]) {
+  let matching = 0;
+
+  messages.forEach((message) => {
+    if (possible.includes(message)) {
+      matching++;
+    }
+  });
+
+  return matching;
+}
+
+console.log(countMatching(parseIntoStrings(getPossibleStrings(0)), messages));
