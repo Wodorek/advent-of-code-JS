6e10a6590edd9c966cbfe706618417514c41f2e9
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-05-28 17:23:56 +0200

    Redo

--- 2020/23/23a.ts
+++ 2020/23/23a.ts
@@ -1,64 +1,31 @@
-export default {};
+class ListNode {
+  val: number;
+  next: ListNode | null;
 
-const cups = '389125467';
-
-let currIdx = 0;
-
-function moveCups(currCups: string, cupIdx: number) {
-  const splitCups = currCups.split('');
-
-  const startFrom = cupIdx + 1 <= splitCups.length - 1 ? cupIdx + 1 : 0;
-
-  const pickUps = [];
-
-  let picking = startFrom;
-
-  for (let i = 0; i < 3; i++) {
-    if (picking > splitCups.length - 1) {
-      picking = 0;
-    }
-    pickUps.push(splitCups[picking]);
-    picking++;
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
   }
-
-  let destination = +currCups[cupIdx] - 1;
-
-  while (pickUps.includes(`${destination}`)) {
-    destination--;
-    if (destination <= 0) {
-      destination = splitCups.length - 1;
-    }
-  }
-
-  console.log(`destination: ${destination}`);
-  console.log(`picks: ${pickUps}`);
-
-  const regString = `[${pickUps[0]} | ${pickUps[1]} | ${pickUps[2]}]`;
-  const regex = new RegExp(regString, 'g');
-
-  const replaced = currCups.replaceAll(regex, '');
-
-  const split = replaced.split(`${destination}`);
-  console.log(split);
-
-  currIdx++;
-  if (currIdx > cups.length - 1) {
-    currIdx = 0;
-  }
-
-  const left = split[0] ? split[0] : '';
-  const right = split[1] ? split[1] : '';
-
-  return left + `${destination}` + pickUps.join('') + right;
 }
 
-let movingCups = cups;
+class LinkedList {
+  head: ListNode | null = null;
 
-for (let i = 0; i < 10; i++) {
-  movingCups = moveCups(movingCups, currIdx);
-  console.log(`current move ${i + 1}`);
-  console.log(movingCups);
-  console.log('');
+  addNode(val: number) {
+    if (!this.head) {
+      this.head = new ListNode(val);
+    } else {
+      const temp = this.head;
+      const node = new ListNode(val);
+      this.head = node;
+      node.next = temp;
+    }
+  }
 }
 
-console.log(movingCups);
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+console.log(list);
