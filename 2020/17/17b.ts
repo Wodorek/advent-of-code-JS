7bca0232eea194a6d295aa0e2d21007134285775
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-06 20:48:39 +0200

    2020 day 17 part 2

--- 2020/17/17b.ts
+++ 2020/17/17b.ts
@@ -1,96 +1,101 @@
 import input from './input';
 
-import prepareInput from './helpers/prepareInput';
-import countActiveAround from './helpers/countActiveAround';
+const inputArr = input.split('\n').map((el) => el.split(''));
 
-const inputArr = prepareInput(input);
-
-function expandCube(cube: string[][][]) {
-  const fillLength = cube[0][0].length;
-
-  const fillRow = new Array(fillLength);
-  fillRow.fill('.');
-
-  const fillSlice: string[][] = [];
-  fillSlice.length = fillLength;
-
-  fillSlice.fill(fillRow);
-
-  const newCube = [fillSlice, ...cube, fillSlice];
-
-  const finalCube = newCube.map((slice) => {
-    const newRow = ['.', ...fillRow, '.'];
-
-    return [
-      newRow,
-      ...slice.map((el) => {
-        return ['.', ...el, '.'];
-      }),
-      newRow,
-    ];
-  });
-  return finalCube;
+interface CubesMap {
+  [key: string]: boolean;
 }
 
-function countActive(cube: string[][][]) {
-  let total = 0;
+const cubesMap: CubesMap = {};
 
-  cube.forEach((slice) => {
-    slice.forEach((row) => {
-      row.forEach((voxel) => {
-        if (voxel === '#') {
-          total++;
-        }
-      });
-    });
-  });
+const cubeSize = 1 + 6 * 2;
+const minimal = cubeSize * -1;
 
-  return total;
-}
+for (let q = minimal; q <= cubeSize; q++) {
+  for (let z = minimal; z <= cubeSize; z++) {
+    for (let y = minimal; y <= cubeSize; y++) {
+      for (let x = minimal; x <= cubeSize; x++) {
+        const id = `${q},${z},${y},${x}`;
 
-function processCube(cube: string[][][]) {
-  const expandedCube = expandCube(cube);
-
-  const newCube = expandedCube.map((slice, sliceIdx) => {
-    return slice.map((row, rowIdx) => {
-      return row.map((voxel, voxelIdx) => {
-        const onAround = countActiveAround(
-          expandedCube,
-          sliceIdx,
-          rowIdx,
-          voxelIdx
-        );
-
-        if (voxel === '#') {
-          if (onAround === 2 || onAround === 3) {
-            return '#';
-          } else {
-            return '.';
-          }
-        } else {
-          if (onAround === 3) {
-            return '#';
-          } else {
-            return '.';
-          }
-        }
-      });
-    });
-  });
-
-  return newCube;
-}
-
-let currCube = inputArr;
-
-for (let i = 0; i < 1000; i++) {
-  currCube = processCube(currCube);
-  const active = countActive(currCube);
-
-  if (active === 848) {
-    console.log(i);
-    break;
+        cubesMap[id] = false;
+      }
+    }
   }
 }
 
-console.log(countActive(currCube));
+//change the -1 here
+inputArr.forEach((column, cidx) => {
+  column.forEach((row, ridx) => {
+    const id = `0,0,${cidx - 3},${ridx - 3}`;
+
+    if (row === '#') {
+      cubesMap[id] = true;
+    }
+  });
+});
+
+function findNeighbors(position: string, cubesMap: CubesMap) {
+  const [q, z, y, x] = position.split(',').map(Number);
+
+  let totalBlack = 0;
+  for (let h = q - 1; h <= q + 1; h++) {
+    for (let i = z - 1; i <= z + 1; i++) {
+      for (let j = y - 1; j <= y + 1; j++) {
+        for (let k = x - 1; k <= x + 1; k++) {
+          const neighborId = `${h},${i},${j},${k}`;
+
+          if (cubesMap[neighborId] === true && neighborId !== position) {
+            totalBlack++;
+          }
+        }
+      }
+    }
+  }
+
+  return totalBlack;
+}
+
+function countBlack(cubesMap: CubesMap) {
+  let totalBlack = 0;
+
+  Object.keys(cubesMap).forEach((key) => {
+    if (cubesMap[key] === true) {
+      totalBlack++;
+    }
+  });
+
+  return totalBlack;
+}
+
+function processCubes(cubesMap: CubesMap) {
+  const allCubes = Object.keys(cubesMap);
+
+  const newMap: CubesMap = {};
+
+  allCubes.forEach((cube) => {
+    const neigborsCount = findNeighbors(cube, cubesMap);
+
+    if (cubesMap[cube] === true) {
+      if (neigborsCount === 2 || neigborsCount === 3) {
+        newMap[cube] = true;
+      } else [(newMap[cube] = false)];
+    } else {
+      if (neigborsCount === 3) {
+        newMap[cube] = true;
+      } else {
+        newMap[cube] = false;
+      }
+    }
+  });
+
+  return newMap;
+}
+
+let currentMap = cubesMap;
+
+for (let i = 0; i < 6; i++) {
+  currentMap = processCubes(currentMap);
+  console.log('cur', i);
+}
+
+console.log(countBlack(currentMap));
