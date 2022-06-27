772a4bb164f42b116f612eba6e9b8040b3cf3b13
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-28 01:12:30 +0200

    Update 14a.ts

--- 2019/14/14a.ts
+++ 2019/14/14a.ts
@@ -4,4 +4,46 @@ import prepareInput from './helpers/prepareInput';
 
 const inputArr = prepareInput(input);
 
-console.log(inputArr);
+const productionLookup: {
+  [key: string]: {
+    ingredients: [number, string][];
+
+    prodQuant: number;
+  };
+} = {};
+
+const leftovers: { [key: string]: number } = {};
+
+inputArr.forEach((reaction) => {
+  const produces = reaction[1][1];
+  const productionQuant = reaction[1][0];
+
+  const ingredients: [number, string][] = [];
+
+  reaction[0].forEach((ing) => {
+    ingredients.push([ing[0], ing[1]] as [number, string]);
+  });
+
+  leftovers[produces] = 0;
+
+  productionLookup[produces] = {
+    ingredients,
+    prodQuant: productionQuant,
+  };
+});
+
+const basicsIngredients: string[] = [];
+
+for (let key in productionLookup) {
+  if (productionLookup[key].ingredients[0][1] === 'ORE') {
+    basicsIngredients.push(key);
+  }
+}
+
+function produceChemical(chemical: string, quantity: number) {
+  const requiredIngrediends = productionLookup[chemical].ingredients;
+
+  console.log(requiredIngrediends, productionLookup[chemical].prodQuant);
+}
+
+produceChemical('FUEL', 1);
