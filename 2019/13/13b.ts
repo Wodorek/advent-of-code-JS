cc02445a1c0fecc6ed9f37c593bca00d908e61e3
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-27 18:23:08 +0200

    2019 day 13 part 2

--- 2019/13/13b.ts
+++ 2019/13/13b.ts
@@ -2,11 +2,11 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 import { VM } from '../computer/VM';
-import chunkArray from './helpers/chunkArray';
 
 const inputArr = prepareInput(input);
 
 const vm = new VM(inputArr);
+vm.memory[0] = 2;
 
 const tileTypes = {
   0: '.',
@@ -16,48 +16,52 @@ const tileTypes = {
   4: 'O',
 };
 
-while (vm.working) {
-  vm.executeInstruction();
-
-  if (vm.outputs.length % 3 === 0) {
-  }
-}
-
-const gridInfo = chunkArray(vm.outputs, 3);
-
-let maxWidth = -Infinity;
-let maxHeight = -Infinity;
-
-gridInfo.forEach((tile) => {
-  maxWidth = Math.max(maxHeight, tile[0]);
-  maxHeight = Math.max(maxHeight, tile[1]);
-});
-
 const grid: string[][] = [];
 
-for (let i = 0; i < maxHeight + 1; i++) {
+for (let i = 0; i < 20 + 1; i++) {
   const arr = new Array();
-  arr.length = maxWidth + 1;
+  arr.length = 37 + 1;
   arr.fill('.');
   grid.push(arr);
 }
 
-gridInfo.forEach((tile) => {
-  grid[tile[1]][tile[0]] = tileTypes[tile[2] as keyof typeof tileTypes];
-});
+let outputs = 0;
 
-grid.forEach((line) => {
-  console.log(line.join(''));
-});
+let ballPosition = 0;
+let paddlePosition = 0;
 
-let totalBlocks = 0;
+let joystickPosition = 0;
 
-grid.forEach((line) => {
-  line.forEach((char) => {
-    if (char === '#') {
-      totalBlocks++;
+while (vm.working) {
+  vm.executeInstruction(joystickPosition);
+
+  if (vm.lastCommand === 4) {
+    outputs++;
+  }
+
+  if (outputs === 3) {
+    const tile = vm.outputs.slice(vm.outputs.length - 3);
+
+    if (tile[0] !== -1) {
+      if (tile[2] === 3) {
+        paddlePosition = tile[0];
+      } else if (tile[2] === 4) {
+        ballPosition = tile[0];
+      }
+
+      if (ballPosition > paddlePosition) {
+        joystickPosition = 1;
+      } else if (ballPosition < paddlePosition) {
+        joystickPosition = -1;
+      } else {
+        joystickPosition = 0;
+      }
+
+      grid[tile[1]][tile[0]] = tileTypes[tile[2] as keyof typeof tileTypes];
+    } else {
+      console.log(`current score ${tile[2]}`);
     }
-  });
-});
 
-console.log(totalBlocks);
+    outputs = 0;
+  }
+}
