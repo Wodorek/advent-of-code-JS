d1e5e7d966bfb4c6d2e3455ca6046049130373be
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-27 17:40:47 +0200

    Better solution

--- 2019/13/13a.ts
+++ 2019/13/13a.ts
@@ -8,6 +8,14 @@ const inputArr = prepareInput(input);
 
 const vm = new VM(inputArr);
 
+const tileTypes = {
+  0: '.',
+  1: '|',
+  2: '#',
+  3: '_',
+  4: 'O',
+};
+
 while (vm.working) {
   vm.executeInstruction();
 
@@ -15,15 +23,41 @@ while (vm.working) {
   }
 }
 
-console.log(vm.outputs);
-
 const gridInfo = chunkArray(vm.outputs, 3);
 
-let totalBlocks = 0;
+let maxWidth = -Infinity;
+let maxHeight = -Infinity;
+
 gridInfo.forEach((tile) => {
-  if (tile[2] === 2) {
-    totalBlocks++;
-  }
+  maxWidth = Math.max(maxHeight, tile[0]);
+  maxHeight = Math.max(maxHeight, tile[1]);
+});
+
+const grid: string[][] = [];
+
+for (let i = 0; i < maxHeight + 1; i++) {
+  const arr = new Array();
+  arr.length = maxWidth + 1;
+  arr.fill('.');
+  grid.push(arr);
+}
+
+gridInfo.forEach((tile) => {
+  grid[tile[1]][tile[0]] = tileTypes[tile[2] as keyof typeof tileTypes];
+});
+
+grid.forEach((line) => {
+  console.log(line.join(''));
+});
+
+let totalBlocks = 0;
+
+grid.forEach((line) => {
+  line.forEach((char) => {
+    if (char === '#') {
+      totalBlocks++;
+    }
+  });
 });
 
 console.log(totalBlocks);
