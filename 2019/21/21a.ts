7c999978319363ba4a9f2031eca111f5f14de83e
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-06-30 18:57:03 +0200

    2019 day 21 part 1

---
+++ 2019/21/21a.ts
@@ -0,0 +1,46 @@
+import input from './input';
+
+import prepareInput from './helpers/prepareInput';
+import { VM } from '../computer/VM';
+import turnIntoCommand from './helpers/turnIntoCommand';
+
+const inputArr = prepareInput(input);
+
+console.log(String.fromCharCode(10));
+
+const vm = new VM(inputArr);
+
+let row: string[] = [];
+
+let inputIdx = 0;
+
+const inputs: number[] = [
+  ...turnIntoCommand('NOT A J'),
+  ...turnIntoCommand('NOT B T'),
+  ...turnIntoCommand('OR T J'),
+  ...turnIntoCommand('NOT C T'),
+  ...turnIntoCommand('OR T J'),
+  ...turnIntoCommand('AND D J'),
+  ...turnIntoCommand('WALK'),
+];
+
+while (vm.working) {
+  vm.executeInstruction(inputs[inputIdx]);
+
+  if (vm.lastCommand === 3) {
+    inputIdx++;
+  }
+
+  if (vm.lastCommand === 4) {
+    const output = vm.getLastOutput;
+
+    if (output === 10) {
+      console.log(row.join(''));
+      row = [];
+    } else {
+      row.push(String.fromCharCode(output));
+    }
+  }
+}
+
+console.log(vm.getLastOutput);
---
+++ 2019/21/helpers/prepareInput.ts
@@ -0,0 +1,4 @@
+function prepareInput(input: string) {
+  return input.split(',').map(Number);
+}
+export default prepareInput;
