4365e7285e756e73eeb8dc566e323de4fc299dc9
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-07-19 15:28:59 +0200

    Update 17a.ts

--- 2018/17/17a.ts
+++ 2018/17/17a.ts
@@ -1,120 +1,67 @@
-import input from './input';
+// import input from './input';
 
-import prepareInput from './helpers/prepareInput';
-import getMin from './helpers/getMin';
-import getMax from './helpers/getMax';
+// import prepareInput from './helpers/prepareInput';
+// import getMin from './helpers/getMin';
+// import getMax from './helpers/getMax';
 
-type coordinate = [string, number, string, number, number];
+// type coordinate = [string, number, string, number, number];
 
-let inputArr = prepareInput(input);
+// let inputArr = prepareInput(input);
 
-const minX = getMin(inputArr, 'x');
-const minY = getMin(inputArr, 'y');
-const spring = 500 - minX;
+// const minX = getMin(inputArr, 'x');
+// const minY = getMin(inputArr, 'y');
+// const spring = 500 - minX;
 
-function reduceArraySize(
-  arr: coordinate[],
-  minX: number,
+// function reduceArraySize(
+//   arr: coordinate[],
+//   minX: number,
 
-  minY: number
-) {
-  const mapped = arr.map((el) => {
-    const mod1 = el[0] === 'x' ? minX : minY;
-    const mod2 = mod1 === minX ? minY : minX;
+//   minY: number
+// ) {
+//   const mapped = arr.map((el) => {
+//     const mod1 = el[0] === 'x' ? minX : minY;
+//     const mod2 = mod1 === minX ? minY : minX;
 
-    return [el[0], el[1] - mod1, el[2], el[3] - mod2, el[4] - mod2];
-  });
+//     return [el[0], el[1] - mod1, el[2], el[3] - mod2, el[4] - mod2];
+//   });
 
-  return mapped as coordinate[];
-}
+//   return mapped as coordinate[];
+// }
 
-const mapped = reduceArraySize(inputArr, minX, minY);
+// const mapped = reduceArraySize(inputArr, minX, minY);
 
-/**Mostly for visualization */
-const createMap = (coordinates: coordinate[]) => {
-  const maxX = getMax(coordinates, 'x');
-  const maxY = getMax(coordinates, 'y');
+// /**Mostly for visualization */
+// const createMap = (coordinates: coordinate[]) => {
+//   const maxX = getMax(coordinates, 'x');
+//   const maxY = getMax(coordinates, 'y');
 
-  const map: string[][] = [];
+//   const map: string[][] = [];
 
-  for (let i = 0; i < maxY + 1; i++) {
-    const row = new Array(maxX + 1).fill('.');
+//   for (let i = 0; i < maxY + 1; i++) {
+//     const row = new Array(maxX + 1).fill('.');
 
-    map.push(row);
-  }
+//     map.push(row);
+//   }
 
-  coordinates.forEach((coord) => {
-    const point = coord[0];
+//   coordinates.forEach((coord) => {
+//     const point = coord[0];
 
-    for (let i = coord[3]; i < coord[4] + 1; i++) {
-      if (point === 'x') {
-        map[i][coord[1]] = '#';
-      } else {
-        map[coord[1]][i] = '#';
-      }
-    }
-  });
+//     for (let i = coord[3]; i < coord[4] + 1; i++) {
+//       if (point === 'x') {
+//         map[i][coord[1]] = '#';
+//       } else {
+//         map[coord[1]][i] = '#';
+//       }
+//     }
+//   });
 
-  return map;
-};
+//   return map;
+// };
 
-const map = createMap(mapped);
+// const map = createMap(mapped);
 
-function pourWater(x: number, y: number) {
-  map[y][x] = 'x';
+const aa = 1;
+const bb = 0;
+const cc = 3;
 
-  let currentBottom = y;
-
-  while (
-    map[currentBottom][x] !== '#' &&
-    map[currentBottom][x] !== '~' &&
-    currentBottom < map.length - 1
-  ) {
-    currentBottom++;
-  }
-
-  currentBottom--;
-
-  const spillQueue: number[] = [];
-
-  while (currentBottom !== y) {
-    map[currentBottom][x] = '|';
-    spillQueue.push(currentBottom);
-    currentBottom--;
-  }
-
-  while (spillQueue.length > 0) {
-    const currentLevel = spillQueue.shift()!;
-
-    let left = x - 1;
-    let right = x + 1;
-
-    while (
-      map[currentLevel][left] !== '#' &&
-      map[currentLevel + 1][left] !== '.'
-    ) {
-      map[currentLevel][left] = '~';
-      left--;
-      if (left === -1) {
-        break;
-      }
-    }
-
-    while (
-      map[currentLevel][right] !== '#' &&
-      map[currentLevel + 1][right] !== '.'
-    ) {
-      map[currentLevel][right] = '~';
-      right++;
-      if (right >= map[0].length) {
-        break;
-      }
-    }
-  }
-}
-
-pourWater(spring + 5, 0);
-
-map.forEach((row) => {
-  console.log(row.join(' '));
-});
+console.log(aa && bb && cc);
