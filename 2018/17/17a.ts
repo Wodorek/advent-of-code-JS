0ffa602fb3f980da21ba4e43ded725fdad871894
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-07-21 18:00:15 +0200

    2017 day 17 part 1

--- 2018/17/17a.ts
+++ 2018/17/17a.ts
@@ -8,23 +8,6 @@ type coordinate = [string, number, string, number, number];
 
 let inputArr = prepareInput(input);
 
-const minX = getMin(inputArr, 'x');
-const minY = getMin(inputArr, 'y');
-const spring = 500 - minX;
-
-const reduceArraySize = (arr: coordinate[], minX: number, minY: number) => {
-  const mapped = arr.map((el) => {
-    const mod1 = el[0] === 'x' ? minX : minY;
-    const mod2 = mod1 === minX ? minY : minX;
-
-    return [el[0], el[1] - mod1, el[2], el[3] - mod2, el[4] - mod2];
-  });
-
-  return mapped as coordinate[];
-};
-
-const mapped = reduceArraySize(inputArr, minX, minY);
-
 const createMap = (coordinates: coordinate[]) => {
   const maxX = getMax(coordinates, 'x');
   const maxY = getMax(coordinates, 'y');
@@ -52,4 +35,90 @@ const createMap = (coordinates: coordinate[]) => {
   return map;
 };
 
-const map = createMap(mapped);
+function hasWall(map: string[][], x: number, y: number, direction: 1 | -1) {
+  let col = x;
+
+  while (true) {
+    if (map[y][col] === '.') {
+      return false;
+    }
+    if (map[y][col] === '#') {
+      return true;
+    }
+
+    col += direction;
+  }
+}
+
+function isContainer(map: string[][], x: number, y: number) {
+  return hasWall(map, x, y, 1) && hasWall(map, x, y, -1);
+}
+
+function flowToSide(map: string[][], x: number, y: number, direction: 1 | -1) {
+  let col = x;
+
+  while (true) {
+    if (map[y][col] === '#') {
+      return;
+    }
+    map[y][col] = '~';
+    col += direction;
+  }
+}
+
+function spreadWater(map: string[][], x: number, y: number) {
+  flowToSide(map, x, y, 1);
+  flowToSide(map, x, y, -1);
+}
+
+function pourWater(
+  map: string[][],
+  x: number,
+  y: number,
+  self: typeof pourWater
+) {
+  if (y >= maxY) {
+    return;
+  }
+
+  if (map[y + 1][x] === '.') {
+    map[y + 1][x] = '|';
+    pourWater(map, x, y + 1, self);
+  }
+
+  if (
+    (map[y + 1][x] === '#' || map[y + 1][x] === '~') &&
+    map[y][x - 1] === '.'
+  ) {
+    map[y][x - 1] = '|';
+    pourWater(map, x - 1, y, self);
+  }
+
+  if (
+    (map[y + 1][x] === '#' || map[y + 1][x] === '~') &&
+    map[y][x + 1] === '.'
+  ) {
+    map[y][x + 1] = '|';
+    pourWater(map, x + 1, y, self);
+  }
+
+  if (isContainer(map, x, y)) {
+    spreadWater(map, x, y);
+  }
+}
+
+const map = createMap(inputArr);
+const maxY = getMax(inputArr, 'y');
+
+pourWater(map, 500, 0, pourWater);
+
+let totalWater = 0;
+
+map.forEach((row) => {
+  row.forEach((char) => {
+    if (char === '|' || char === '~') {
+      totalWater++;
+    }
+  });
+});
+console.log(totalWater);
