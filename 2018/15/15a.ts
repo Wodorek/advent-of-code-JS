c71a3a4b15aa02b1cba1201b8534689a6c317043
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-07-22 19:35:26 +0200

    Pathfinding done

--- 2018/15/15a.ts
+++ 2018/15/15a.ts
@@ -13,19 +13,91 @@ inputArr.forEach((el) => {
 });
 
 class Monster {
-  position: [number, number];
+  position: { x: number; y: number };
   hp: number = 200;
   attack: number = 3;
   type: 'Elf' | 'Goblin';
   enemy: 'Elf' | 'Goblin';
 
   constructor(x: number, y: number, type: 'G' | 'E') {
-    this.position = [x, y];
+    this.position = { x, y };
     this.type = type === 'G' ? 'Goblin' : 'Elf';
     this.enemy = type === 'G' ? 'Elf' : 'Goblin';
   }
 
-  findNextStep(topography: ITopography, enemies: Monster[]) {}
+  move(vector: [number, number]) {
+    this.position.x += vector[0];
+    this.position.y += vector[1];
+  }
+
+  // possibly the worst piece of code I have ever produced.
+  findNextStep(topography: ITopography, enemies: Monster[]) {
+    const visited: string[] = [`${this.position.x}, ${this.position.y}`];
+
+    const enemyPositions = enemies.map((enemy) => {
+      return `${enemy.position.x},${enemy.position.y}`;
+    });
+
+    console.log('enemies', enemyPositions);
+
+    let pathsTaken = [[{ x: this.position.x, y: this.position.y }]];
+
+    const steps = [
+      [0, -1], //up
+      [1, 0], //right
+      [0, 1], //down
+      [-1, 0], //left
+    ];
+
+    //hate do do it
+    while (true) {
+      let newPaths: any[] = [];
+      let targetPaths: any[] = [];
+
+      pathsTaken.forEach((path) => {
+        let adjacents = steps.map((step) => {
+          return {
+            x: path[path.length - 1].x + step[0],
+            y: path[path.length - 1].y + step[1],
+          };
+        });
+
+        adjacents.forEach((adjacent) => {
+          const id = `${adjacent.x},${adjacent.y}`;
+
+          if (enemyPositions.includes(id)) {
+            const enemyPosition = enemyPositions[enemyPositions.indexOf(id)]
+              .split(',')
+              .map(Number);
+
+            targetPaths.push([
+              ...path,
+              adjacent,
+              {
+                x: enemyPosition[0],
+                y: enemyPosition[1],
+              },
+            ]);
+          } else if (!visited.includes(id) && topography[id] === '.') {
+            newPaths.push([...path, adjacent]);
+          }
+          visited.push(id);
+        });
+      });
+      if (targetPaths.length > 0) {
+        targetPaths = targetPaths.sort((p1, p2) =>
+          p1[p1.length - 1].y === p2[p2.length - 1].y
+            ? p1[p1.length - 1].x - p2[p2.length - 1].x
+            : p1[p1.length - 1].y - p2[p2.length - 1].y
+        );
+        return targetPaths[0][1];
+      }
+
+      pathsTaken = newPaths;
+
+      if (pathsTaken.length < 1) return null;
+    }
+  }
 }
 
 class Battlefield {
@@ -53,5 +125,8 @@ class Battlefield {
 }
 
 const battlefield = new Battlefield(inputArr);
+const goblin = battlefield.goblins[0];
 
-console.log(battlefield.goblins);
+console.log(goblin);
+
+console.log(goblin.findNextStep(battlefield.topography, battlefield.elves));
