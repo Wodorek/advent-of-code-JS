b1c5aa3f48a4f21db451eabb41fa23322a0c6641
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-10-18 17:32:45 +0200

    Update prepareInput.ts

--- 2021/08/helpers/prepareInput.ts
+++ 2021/08/helpers/prepareInput.ts
@@ -3,27 +3,15 @@
  * @param input a string of values, separated by a newline
  */
 const prepareInput = (input: string) => {
-  // for real input
   const split = input.split(/\n+/g);
 
-  // //for test input
-  // const split = input.split('\n');
-
   const arr: string[][] = [];
-
-  //real input
 
   for (let i = 0; i < split.length; i++) {
     const element = split[i].split(' | ');
 
     arr.push([element[0], element[1]]);
   }
-
-  // //test input
-
-  // for (let i = 0; i < split.length; i += 2) {
-  //   arr.push([split[i].replace(' |', ''), split[i + 1]]);
-  // }
 
   return arr;
 };
