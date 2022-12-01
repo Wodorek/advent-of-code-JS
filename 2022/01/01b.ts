105ff35b7f2e00e77cd5feb3d5164f058c1f1a66
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-01 11:57:16 +0100

    Day 1 part 2

--- 2022/01/01b.ts
+++ 2022/01/01b.ts
@@ -2,6 +2,16 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputArr = prepareInput(input);
 
-console.log(inputArr)
+const summedUp = inputArr
+  .map((inv) => {
+    return inv.reduce((prev, val) => prev + val, 0);
+  })
+  .sort((a, b) => b - a);
+
+const topThree = summedUp.slice(0, 3);
+
+const topSum = topThree.reduce((prev, val) => prev + val, 0);
+
+console.log(topSum);
