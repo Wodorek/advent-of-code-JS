d6d627890de8b294995fce28b7e2d62fa97fb162
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-02 09:29:27 +0100

    2022 day 2 part 1

--- 2022/02/02a.ts
+++ 2022/02/02a.ts
@@ -2,6 +2,44 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputArr = prepareInput(input);
 
-console.log(inputArr)
+interface IPoints {
+  X: number;
+  Y: number;
+  Z: number;
+}
+interface IRules {
+  A: IPoints;
+  B: IPoints;
+  C: IPoints;
+}
+
+const gameRules: IRules = {
+  A: { X: 3, Y: 6, Z: 0 },
+  B: { X: 0, Y: 3, Z: 6 },
+  C: { X: 6, Y: 0, Z: 3 },
+};
+
+const shapeValues: IPoints = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+function playGame(elfShape: 'A' | 'B' | 'C', myShape: 'X' | 'Y' | 'Z') {
+  let score = 0;
+
+  score += gameRules[elfShape][myShape];
+  score += shapeValues[myShape];
+
+  return score;
+}
+
+let scores = 0;
+
+inputArr.forEach((game) => {
+  scores += playGame(game[0], game[1]);
+});
+
+console.log(scores);
--- 2022/02/helpers/prepareInput.ts
+++ 2022/02/helpers/prepareInput.ts
@@ -1,3 +1,8 @@
-function prepareInput(input:string) {};
+function prepareInput(input: string) {
+  return input.split('\n').map((el) => el.split(' ')) as [
+    'A' | 'B' | 'C',
+    'X' | 'Y' | 'Z'
+  ][];
+}
 
-export default prepareInput
+export default prepareInput;
