28c57eb6a5c2630d1d843d6d031cc71c98dd157e
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-04 15:37:35 +0100

    2022 day 4 part 2

--- 2022/04/04b.ts
+++ 2022/04/04b.ts
@@ -2,6 +2,24 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputArr = prepareInput(input);
 
-console.log(inputArr)
+function checkPair(pair: number[][]) {
+  const [elf1, elf2] = pair;
+
+  if (elf1[1] >= elf2[0] && elf1[0] <= elf2[1]) {
+    return true;
+  }
+
+  return false;
+}
+
+//6,8 2,6
+
+let countedPairs = 0;
+
+inputArr.forEach((pair) => {
+  checkPair(pair) ? countedPairs++ : null;
+});
+
+console.log(countedPairs);
