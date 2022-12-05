f2838acbc8283618b52877111fadc61919c50765
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-05 18:42:48 +0100

    2022 day 5 part 2
    I love when all you have to do for part 2 is to remove one method

--- 2022/05/05b.ts
+++ 2022/05/05b.ts
@@ -2,6 +2,58 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+interface IStacks {
+  [key: number]: string[];
+}
 
-console.log(inputArr)
+const [initialPositions, moves] = prepareInput(input);
+
+function populateStacks(initial: string[][]) {
+  const stacks: IStacks = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+  };
+
+  initial.forEach((line) => {
+    line.forEach((char, idx) => {
+      if (char !== ' ') {
+        if (idx === 1) {
+          stacks[1].push(char);
+        } else {
+          stacks[(idx - 1) / 4 + 1].push(char);
+        }
+      }
+    });
+  });
+
+  return stacks;
+}
+
+const stacks = populateStacks(initialPositions);
+
+function moveItems(stacks: IStacks, move: number[]) {
+  const [size, from, to] = move;
+
+  const itemsToMove = stacks[from].splice(0, size);
+
+  stacks[to].unshift(...itemsToMove);
+}
+
+moves.forEach((move) => {
+  moveItems(stacks, move);
+});
+
+let outcome = '';
+
+Object.values(stacks).forEach((stack) => {
+  outcome = outcome + stack[0];
+});
+
+console.log(outcome);
