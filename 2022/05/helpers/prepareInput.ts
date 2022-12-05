08e3d82f45cc0f1c165f005a3ff298cafe0c1324
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-05 18:41:21 +0100

    2022 day 5 part 1

--- 2022/05/helpers/prepareInput.ts
+++ 2022/05/helpers/prepareInput.ts
@@ -1,3 +1,26 @@
-function prepareInput(input:string) {};
+/**
+ *
+ * @param input input string
+ * @returns initial positions,where first populated cell is at idx of 1, then every 4 spaces and moves formatted as such: [how many, from, where]
+ */
+function prepareInput(input: string) {
+  const [initial, moves] = input.split('\n\n').map((inp) => inp.split('\n'));
 
-export default prepareInput
+  const parsedInitial = initial.map((line) => {
+    return line.replaceAll(new RegExp(/\[|\]/g), ' ').split('');
+  });
+
+  const parsedMoves = moves.map((move) => {
+    return move
+      .replaceAll(new RegExp(/move |from |to /g), '')
+      .split(' ')
+      .map(Number);
+  });
+
+  return [parsedInitial.slice(0, parsedInitial.length - 1), parsedMoves] as [
+    string[][],
+    number[][]
+  ];
+}
+
+export default prepareInput;
