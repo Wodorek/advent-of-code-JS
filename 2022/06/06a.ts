e576ddc0d1200fea82a8346f6a8469e9b2f3f4a8
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-06 15:42:04 +0100

    2022 day 6 part 1

--- 2022/06/06a.ts
+++ 2022/06/06a.ts
@@ -2,6 +2,19 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputStr = prepareInput(input);
 
-console.log(inputArr)
+function findMarker(buffer: string) {
+  let currIdx = 0;
+
+  while (currIdx < buffer.length) {
+    const slice = new Set(buffer.slice(currIdx, currIdx + 4));
+
+    if (slice.size === 4) {
+      return currIdx + 4;
+    }
+    currIdx++;
+  }
+}
+
+console.log(findMarker(inputStr));
--- 2022/06/helpers/prepareInput.ts
+++ 2022/06/helpers/prepareInput.ts
@@ -1,3 +1,10 @@
-function prepareInput(input:string) {};
+/**
+ * Yes, this is pointless, i'm keeping it for the sake of keeping up the formula
+ * @param input
+ * @returns
+ */
+function prepareInput(input: string) {
+  return input;
+}
 
-export default prepareInput
+export default prepareInput;
