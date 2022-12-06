ee00160ad37df4dcde388bb5e400eecd565144c2
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-12-06 15:42:53 +0100

    2022 day 6 part 2

--- 2022/06/06b.ts
+++ 2022/06/06b.ts
@@ -2,6 +2,19 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputStr = prepareInput(input);
 
-console.log(inputArr)
+function findMarker(buffer: string) {
+  let currIdx = 0;
+
+  while (currIdx < buffer.length) {
+    const slice = new Set(buffer.slice(currIdx, currIdx + 14));
+
+    if (slice.size === 14) {
+      return currIdx + 14;
+    }
+    currIdx++;
+  }
+}
+
+console.log(findMarker(inputStr));
