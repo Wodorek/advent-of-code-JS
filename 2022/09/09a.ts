3693d73190faebfb5b144cea875e4292187f7ead
Wodorek <Krol.w94@gmail.com>
2022-12-29 14:23:18 +0100

    2022 day 9 part 1

--- 2022/09/09a.ts
+++ 2022/09/09a.ts
@@ -6,26 +6,14 @@ const inputArr = prepareInput(input);
 
 const moves = {
   STOP: { x: 0, y: 0 },
-  D: { x: 0, y: -1 },
-  U: { x: 0, y: 1 },
+  D: { x: 0, y: 1 },
+  U: { x: 0, y: -1 },
   R: { x: 1, y: 0 },
   L: { x: -1, y: 0 },
-  DR: { x: 1, y: -1 },
-  DL: { x: 1, y: 1 },
+  DR: { x: 1, y: 1 },
+  DL: { x: -1, y: 1 },
   UL: { x: -1, y: -1 },
-  UR: { x: -1, y: 1 },
-};
-
-const opposites = {
-  STOP: 'STOP',
-  D: 'U',
-  U: 'D',
-  L: 'R',
-  R: 'L',
-  UR: 'DL',
-  DL: 'UR',
-  UL: 'DR',
-  DR: 'UL',
+  UR: { x: 1, y: -1 },
 };
 
 interface IKnot {
@@ -35,21 +23,82 @@ interface IKnot {
 
 class Rope {
   head: IKnot = {
-    x: 2,
-    y: 1,
+    x: 0,
+    y: 0,
   };
   tail: IKnot = {
     x: 0,
     y: 0,
   };
 
-  nextMove(leader: IKnot, follower: IKnot): keyof typeof moves {
+  tailPositions = new Set<string>();
+
+  findNextMove(leader: IKnot, follower: IKnot): keyof typeof moves {
     const dx = leader.x - follower.x;
     const dy = leader.y - follower.y;
 
-    console.table({ dx, dy });
+    if (Math.abs(dx) === 1 && Math.abs(dy) === 1) {
+      return 'STOP';
+    }
+
+    if (dx === 0) {
+      if (dy > 1) {
+        return 'D';
+      } else if (dy < -1) {
+        return 'U';
+      } else {
+        return 'STOP';
+      }
+    }
+
+    if (dy === 0) {
+      if (dx > 1) {
+        return 'R';
+      } else if (dx < -1) {
+        return 'L';
+      } else {
+        return 'STOP';
+      }
+    }
+
+    if (dx > 0) {
+      if (dy > 0) {
+        return 'DR';
+      } else {
+        return 'UR';
+      }
+    }
+
+    if (dx < 0) {
+      if (dy > 0) {
+        return 'DL';
+      } else {
+        return 'UL';
+      }
+    }
 
     return 'STOP';
+  }
+
+  processMove(
+    leader: IKnot,
+    follower: IKnot,
+    leaderMoves: { dir: string; steps: number }
+  ) {
+    const { steps } = leaderMoves;
+    const dir = leaderMoves.dir as keyof typeof moves;
+
+    for (let i = 0; i < steps; i++) {
+      leader.x += moves[dir].x;
+      leader.y += moves[dir].y;
+
+      const nextMove = this.findNextMove(leader, follower);
+
+      follower.x += moves[nextMove].x;
+      follower.y += moves[nextMove].y;
+
+      this.tailPositions.add(`${this.tail.x},${this.tail.y}`);
+    }
   }
 
   move(knot: IKnot, dir: keyof typeof moves) {
@@ -60,20 +109,10 @@ class Rope {
 
 const rope = new Rope();
 
-rope.nextMove(rope.head, rope.tail);
+inputArr.forEach((directive) => {
+  const [dir, steps] = directive;
 
-const knotMap: string[][] = [];
-
-for (let i = 0; i < 10; i++) {
-  knotMap.push(['.', '.', '.', '.', '.', '.', '.', '.', '.', '.']);
-}
-
-const { x: hx, y: hy } = rope.head;
-const { x: tx, y: ty } = rope.tail;
-
-knotMap[hy][hx] = 'H';
-knotMap[ty][tx] = 'T';
-
-knotMap.forEach((line) => {
-  console.log(line.join(' '));
+  rope.processMove(rope.head, rope.tail, { dir, steps });
 });
+
+console.log(rope.tailPositions.size);
