94a2b6f1439fc3507affa0b86addc72f9068cd3d
Wodorek <Krol.w94@gmail.com>
2022-12-31 20:15:16 +0100

    2022 day 10 part 2 WIP

--- 2022/10/10b.ts
+++ 2022/10/10b.ts
@@ -2,6 +2,61 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputArr = prepareInput(input);
 
-console.log(inputArr)
+class Computer {
+  operations: [string, number][] = [];
+  currentCycle = 1;
+  register = 1;
+  currRow = 0;
+  signals: number[] = [];
+  crt: string[][] = [];
+
+  constructor(ops: [string, number][]) {
+    this.operations = ops;
+
+    for (let i = 0; i <= 5; i++) {
+      const row: string[] = [];
+
+      for (let j = 0; j <= 39; j++) {
+        row.push('.');
+      }
+
+      this.crt.push(row);
+    }
+  }
+
+  display() {
+    this.crt.forEach((row) => {
+      console.log(row.join(' '));
+    });
+  }
+
+  checkSignal() {}
+
+  processCycle(op: string, num: number) {
+    if (op === 'noop') {
+      this.currentCycle++;
+      this.checkSignal();
+    }
+
+    if (op === 'addx') {
+      this.checkSignal();
+      this.currentCycle++;
+      this.checkSignal();
+      this.currentCycle++;
+      this.checkSignal();
+      this.register += num;
+    }
+  }
+
+  runProgram() {
+    this.operations.forEach((operation) => {
+      this.processCycle(...operation);
+    });
+  }
+}
+
+const computer = new Computer(inputArr);
+computer.runProgram();
+computer.display();
