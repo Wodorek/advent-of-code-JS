6d162e6c194822ecb51cb3e7527138f6d3acb6d5
Wodorek <Krol.w94@gmail.com>
2023-01-01 15:39:37 +0100

    2022 day 11 part 1 wip

--- 2022/11/11a.ts
+++ 2022/11/11a.ts
@@ -2,6 +2,7 @@ import input from './input';
 
 import prepareInput from './helpers/prepareInput';
 
-const inputArr = prepareInput(input)
+const inputArr = prepareInput(input);
 
-console.log(inputArr)
+console.log(inputArr);
+console.log(inputArr[0].test(23));
--- 2022/11/helpers/prepareInput.ts
+++ 2022/11/helpers/prepareInput.ts
@@ -1,3 +1,33 @@
-function prepareInput(input:string) {};
+export interface IMonkey {
+  id: number;
+  starting: number[];
+  operation: (old: number) => number;
+  test: (num: number) => number;
+}
 
-export default prepareInput
+function factory(var1: string, var2: string, op: string) {
+  if (isNaN(+var2)) {
+  }
+}
+
+function prepareInput(input: string) {
+  const split = input.split('\n\n').map((inp) => inp.split('\n'));
+
+  return split.map((el) => {
+    const monkey: IMonkey = {
+      id: +el[0].split(' ')[1].replace(':', ''),
+      starting: eval(`[${el[1].replace('Starting items: ', '')}]`),
+      test: (num: number) => {
+        if (num % +el[3].split(' ').reverse()[0] === 0) {
+          return +el[4].split(' ').reverse()[0];
+        } else {
+          return +el[5].split(' ').reverse()[0];
+        }
+      },
+    };
+
+    return monkey;
+  });
+}
+
+export default prepareInput;
