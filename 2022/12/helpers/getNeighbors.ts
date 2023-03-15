8144024018e65d6840872ac0ba6ce43b3d6c5057
Wodorek <58212786+Wodorek@users.noreply.github.com>
2023-03-15 13:39:20 +0100

    Create getNeighbors.ts

---
+++ 2022/12/helpers/getNeighbors.ts
@@ -0,0 +1,9 @@
+function getNeighbors(row: number, col: number, terrain: string[][]) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const neighbors = [];
+
+  const currElevation = terrain[row][col];
+  const currElevationIdx = alphabet.indexOf(currElevation);
+}
+
+export default getNeighbors;
