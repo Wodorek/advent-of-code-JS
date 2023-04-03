f30fe9f71541e57e0f7c381abcef940c3fc43466
Wodorek <58212786+Wodorek@users.noreply.github.com>
2023-04-03 19:06:41 +0200

    Update getNeighbors.ts

--- 2022/12/helpers/getNeighbors.ts
+++ 2022/12/helpers/getNeighbors.ts
@@ -1,9 +1,8 @@
 function getNeighbors(row: number, col: number, terrain: string[][]) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  const neighbors = [];
-
-  const currElevation = terrain[row][col];
-  const currElevationIdx = alphabet.indexOf(currElevation);
+  // const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  // const neighbors = [];
+  // const currElevation = terrain[row][col];
+  // const currElevationIdx = alphabet.indexOf(currElevation);
 }
 
 export default getNeighbors;
